18e370118c02b9d708545c2bd8de5bc5e34360a4
GaborBanfi <banfi.gabor.90@gmail.com>
2018-06-17 12:56:25 +0200

    initial commit

---
+++ src/main/java/com/garage/logforging/LogforgingApplication.java
@@ -0,0 +1,12 @@
+package com.garage.logforging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogforgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogforgingApplication.class, args);
+	}
+}
