4b7641b0e06631e86a6b168bee501d3c27cb3373
GaborBanfi <banfi.gabor.90@gmail.com>
2018-08-05 20:42:58 +0200

    memory leaking code
    +pre & post results

--- src/main/java/com/garage/logforging/controller/BankingAPI.java
+++ src/main/java/com/garage/logforging/controller/BankingAPI.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 @RestController
@@ -28,10 +30,23 @@ public class BankingAPI {
 
     @PostMapping("/balance")
     @ResponseBody
-    public Balance logforgingAllowingEndpoint(@RequestBody String guid) {
+    public Balance logforgingAllowingEndpoint(@RequestBody String guid) throws Exception {
+        memoryLeak();
         return bankingService.getBalanceOfUser(guid);
         // logged message
         // 22613 [http-nio-8080-exec-2] INFO  com.garage.logforging.service.BankingService  - Balance is requested for user id = 16\r\n2018-06-17 16:32:54.494  INFO 6416 --- [nio-8080-exec-4] c.g.logforging.service.BankingService    :  java.lang.NullPointerException
+    }
+
+    private void memoryLeak() throws Exception {
+        Class unsafeClass = Class.forName("sun.misc.Unsafe");
+        Field f = unsafeClass.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        try {
+            unsafe.allocateMemory(100 * 1024 * 1024); // 100 MB memory
+        } catch (Error e) {
+            e.printStackTrace();
+        }
     }
 
     @PostMapping("/secure/balance")
